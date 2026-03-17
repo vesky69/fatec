b5f8d52c694252151ab12a0ac28190d0860bddf3
Júlia :) <148802056+vesky69@users.noreply.github.com>
2026-03-17 12:07:55 +0000

    INSS

---
+++ inss/script.js
@@ -0,0 +1,13 @@
+/**
+ * @author Júlia Dias <jdoc173@gmail.com>
+ * @since 2026-03-17
+ * @version 1.0.0
+ * @description Esta função calcula o INSS.
+ */
+
+function INSS(event) {
+    event.preventDefault();
+
+
+
+}
